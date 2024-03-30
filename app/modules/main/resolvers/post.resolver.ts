6600dac75a4a6273e783d38774dcdd48ade3b1f9
Vladimir Durana <durana.vladimir99@gmail.com>
2024-03-30 18:38:11 -0400

    feat: add auth resolve field feature

--- app/modules/main/resolvers/post.resolver.ts
+++ app/modules/main/resolvers/post.resolver.ts
@@ -1,12 +1,13 @@
 import { ZodArgs } from 'nestjs-graphql-zod'
-import { Mutation, Query, Resolver } from '@nestjs/graphql'
+import { Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
 import { PostCreateSchema } from '../schemas/post.schemas.js'
 import type { PostCreateType } from '../schemas/post.schemas.js'
 import Post from '../models/post.js'
 import { PostPaginated } from '../dtos/post.dto.js'
 import { PaginationScheme, type PaginationType } from '../schemas/common.schemas.js'
+import User from '../models/user.js'
 
-@Resolver()
+@Resolver(() => Post)
 export class PostResolver {
   @Mutation(() => Post)
   async createPost(@ZodArgs(PostCreateSchema) postData: PostCreateType) {
@@ -16,5 +17,10 @@ export class PostResolver {
   @Query(() => PostPaginated)
   async findPosts(@ZodArgs(PaginationScheme) paginationProps: PaginationType) {
     return (await Post.query().paginate(paginationProps.page, paginationProps.perPage)).serialize()
+  }
+
+  @ResolveField(() => [Post])
+  async author(@Parent() post: Post) {
+    return User.findOrFail(post.userId)
   }
 }
